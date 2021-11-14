30c19322f3730849c817c7da727082520e110ae6
Matej Mrenica <matejm98mthw@gmail.com>
2021-11-14 12:31:50 +0100

    Opraveny DB seeder, server je odteraz cez docker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        MiestoSeeder::class;
-        UserSeeder::class;
+        $this->call([
+        MiestoSeeder::class,
+        UserSeeder::class]);
     }
 }
--- public/js/iframe.js
+++
@@ -1,5 +0,0 @@
-$("#map").on("load", function() {
-    let head = $("#map").contents().find("head");
-    let css = '<style>body {padding: 10px !important;}</style>';
-    $(head).append(css);
-});
